52261fd4acb3d8b71374830935509faa59bf1e55
Sandro <sandrocoelho2023a@gmail.com>
2025-08-13 22:28:41 -0300

    inserindo layout.html

--- tempo/tempo/settings.py
+++ tempo/tempo/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    't2',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    't2',
 ]
 
 MIDDLEWARE = [
